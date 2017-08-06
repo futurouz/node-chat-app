2e19f642e0338af7db62ad0ecdabecb303fb1581
futurouz <kunanan.tas@mail.kmutt.ac.th>
2017-08-06 22:45:09 +0700

    add form and send other tabs

--- public/js/index.js
+++ public/js/index.js
@@ -9,10 +9,21 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log(message)
+    console.log('New message', message)
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    
+    jQuery('#messages').append(li);
 });
 
 
-// socket.on('newEmail', function (email) {
-//     console.log('New email', email)
-// });
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
+});
--- server/server.js
+++ server/server.js
@@ -18,10 +18,10 @@ io.on('connection', (socket) => {
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('Create Message ', message)
         io.emit('newMessage', generateMessage(message.from, message.text));
-
+        callback('This is from server')
         // socket.broadcast.emit('newMessage',{
         //     from: message.from,
         //     text: message.text,
